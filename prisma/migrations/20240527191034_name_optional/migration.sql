02599f8d9a8ae60dcd920f16bad532b3cb4620e0
Franz Münzner <jurifm@icloud.com>
2024-06-05 19:48:38 +0200

    migrations

---
+++ prisma/migrations/20240527191034_name_optional/migration.sql
@@ -0,0 +1,16 @@
+-- RedefineTables
+PRAGMA foreign_keys=OFF;
+CREATE TABLE "new_User" (
+    "id" TEXT NOT NULL PRIMARY KEY,
+    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" DATETIME NOT NULL,
+    "username" TEXT NOT NULL,
+    "name" TEXT,
+    "password" TEXT NOT NULL
+);
+INSERT INTO "new_User" ("createdAt", "id", "name", "password", "updatedAt", "username") SELECT "createdAt", "id", "name", "password", "updatedAt", "username" FROM "User";
+DROP TABLE "User";
+ALTER TABLE "new_User" RENAME TO "User";
+CREATE UNIQUE INDEX "User_username_key" ON "User"("username");
+PRAGMA foreign_key_check("User");
+PRAGMA foreign_keys=ON;
---
+++ prisma/migrations/20240529124849_admin_privilege/migration.sql
@@ -0,0 +1,15 @@
+-- RedefineTables
+PRAGMA foreign_keys=OFF;
+CREATE TABLE "new_User" (
+    "id" TEXT NOT NULL PRIMARY KEY,
+    "username" TEXT NOT NULL,
+    "name" TEXT,
+    "password_hash" TEXT NOT NULL,
+    "admin" BOOLEAN NOT NULL DEFAULT false
+);
+INSERT INTO "new_User" ("id", "name", "password_hash", "username") SELECT "id", "name", "password_hash", "username" FROM "User";
+DROP TABLE "User";
+ALTER TABLE "new_User" RENAME TO "User";
+CREATE UNIQUE INDEX "User_username_key" ON "User"("username");
+PRAGMA foreign_key_check("User");
+PRAGMA foreign_keys=ON;
