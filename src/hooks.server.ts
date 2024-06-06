8cdac255f8297044ddfef9237e028284e188b860
Franz Münzner <jurifm@icloud.com>
2024-06-06 17:51:50 +0200

    settings page in progress

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -51,8 +51,12 @@ const authorization: Handle = async ({ event, resolve }) => {
         return redirect(303, "/");
     }
 
-    if (event.url.pathname.startsWith("admin") && !event.locals.user?.admin) {
+    if (event.url.pathname.startsWith("/admin") && !event.locals.user?.admin) {
         return redirect(303, "/");
+    }
+
+    if (event.url.pathname === "/settings") {
+        return redirect(303, "/settings/profile");
     }
 
     return resolve(event);
