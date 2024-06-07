908902ce57327a6edb8c471853f8a7aef717f09b
Franz Münzner <jurifm@icloud.com>
2024-06-07 17:24:10 +0200

    adjusted alert button light mode

--- src/lib/styles.ts
+++ src/lib/styles.ts
@@ -2,7 +2,7 @@ export const defaultButtonStyle: string =
     "bg-black text-white border border-black hover:bg-white hover:text-black hover:border hover:border-black dark:bg-white dark:text-black dark:border dark:border-white dark:hover:bg-black dark:hover:text-white dark:hover:border dark:hover:border-white";
 
 export const alertButtonStyle: string =
-    "bg-red-600 dark:bg-red-600 text-white dark:text-black hover:text-white dark:hover:text-red-600 dark:hover:bg-black dark:border dark:hover:border dark:border-red-600 dark:hover:border-red-600";
+    "bg-red-600 dark:bg-red-600 text-white dark:text-black border border-red-600 dark:border dark:hover:border dark:border-red-600 dark:hover:border-red-600 hover:text-red-600 hover:border-red-600 hover:bg-white dark:hover:text-red-600 dark:hover:bg-black";
 
 export const defaultInputStyle: string =
     "focus:ring-black focus:ring-2 dark:focus:ring-1 dark:focus:ring-white bg-white dark:bg-black";
