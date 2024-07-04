8464f54ac4b8d75a07ac868d20b0c712d28376d1
Franz Münzner <jurifm@icloud.com>
2024-07-04 11:55:45 +0200

    added info to graph

--- src/routes/graph/+page.svelte
+++ src/routes/graph/+page.svelte
@@ -15,6 +15,8 @@ import {
 } from "d3";
 import { dark } from "$lib/stores";
 import { get } from "svelte/store";
+import { Button, Popover } from "flowbite-svelte";
+import QuestionCircleOutline from "flowbite-svelte-icons/QuestionCircleOutline.svelte";
 
 let d3 = {
     zoom,
@@ -30,6 +32,7 @@ let d3 = {
     forceCenter,
 };
 let svg;
+let container;
 let width = 0;
 let height = 0;
 const nodeRadius = 6;
@@ -121,40 +124,45 @@ function resize() {
 
 <svelte:window on:resize={resize} />
 
-<svg bind:this={svg} class="w-full h-full">
-    {#each links as link}
-        <g stroke="#999" stroke-opacity="0.6">
-            <line
-                x1={link.source.x}
-                y1={link.source.y}
-                x2={link.target.x}
-                y2={link.target.y}
-                transform="translate({transform.x} {transform.y}) scale({transform.k} {transform.k})"
-            >
-                <title>{link.source.id}</title>
-            </line>
-        </g>
-    {/each}
+<div class="w-full h-full" bind:this={container}>
+    <Button id="what" class="absolute !p-2 !mt-4 bg-black border border-black text-white hover:text-black dark:bg-white dark:border dark:border-white dark:text-black dark:hover:text-white dark:hover:bg-black" pill={true}><QuestionCircleOutline size="sm" /></Button>
+    <Popover class="[&_div]:bg-white [&_div]:border-black w-80 text-sm font-light py-2 px-3 bg-white shadow text-black dark:bg-black dark:border dark:text-white dark:[&_div]:bg-black" title="what's this?" triggeredBy="#what" placement="bottom-end">one day i realized i completely lost track of who is friends with who, so i made this graph (work in progress). the only criteria at the moment is that that people spend time with each other apart from a school context.</Popover>
+    <svg bind:this={svg} class="w-full h-full">
+        {#each links as link}
+            <g stroke="#999" stroke-opacity="0.6">
+                <line
+                    x1={link.source.x}
+                    y1={link.source.y}
+                    x2={link.target.x}
+                    y2={link.target.y}
+                    transform="translate({transform.x} {transform.y}) scale({transform.k} {transform.k})"
+                >
+                    <title>{link.source.id}</title>
+                </line>
+            </g>
+        {/each}
+    
+        {#each nodes as point}
+            <g class="node">
+                <circle
+                    fill={color}
+                    r="6"
+                    cx={point.x}
+                    cy={point.y}
+                    transform="translate({transform.x} {transform.y}) scale({transform.k} {transform.k})"
+                />
+                <text
+                    fill={color}
+                    x={point.x}
+                    y={point.y + 15}
+                    transform="translate({transform.x} {transform.y}) scale({transform.k} {transform.k})"
+                    text-anchor="middle"
+                    font-size="10px"
+                >
+                    {point.id}
+                </text>
+            </g>
+        {/each}
+    </svg>
+</div>
 
-    {#each nodes as point}
-        <g class="node">
-            <circle
-                fill={color}
-                r="6"
-                cx={point.x}
-                cy={point.y}
-                transform="translate({transform.x} {transform.y}) scale({transform.k} {transform.k})"
-            />
-            <text
-                fill={color}
-                x={point.x}
-                y={point.y + 15}
-                transform="translate({transform.x} {transform.y}) scale({transform.k} {transform.k})"
-                text-anchor="middle"
-                font-size="10px"
-            >
-                {point.id}
-            </text>
-        </g>
-    {/each}
-</svg>
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -3,6 +3,7 @@ const config = {
         "./src/**/*.{html,js,svelte,ts}",
         "./node_modules/flowbite-svelte/**/*.{html,js,svelte,ts}",
         "./node_modules/flowbite-svelte-blocks/**/*.{html,js,svelte,ts}",
+        "./node_modules/flowbite-svelte-icons/**/*.{html,js,svelte,ts}",
     ],
 
     plugins: [require("flowbite/plugin"), require("flowbite-typography")],
